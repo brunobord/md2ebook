38a193753e25286ef928ade38bc9b3a4f04b57d3
Bruno Bord <bruno@jehaisleprintemps.net>
2013-11-24 00:02:51 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
+from os.path import join, dirname, abspath
+
+
+def read_relative_file(filename):
+    """Returns contents of the given file, whose path is supposed relative
+    to this module."""
+    with open(join(dirname(abspath(__file__)), filename)) as f:
+        return f.read()
 
 setup(
     name='md2ebook',
-    version='0.1',
+    version='0.1.1',
     description='Build e-books (EPUB or PDF) out of markdown files',
-    long_description=open('README.rst').read(),
+    long_description=read_relative_file('README.rst'),
     url='https://github.com/brunobord/md2ebook/',
     license='MIT',
     author='Bruno Bord',
